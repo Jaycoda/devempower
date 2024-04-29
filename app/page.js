44e51c256c258fafbc1cdeb1100cf0eb307e5903
jaycoda <jaymarketer@gmail.com>
2024-04-29 14:04:43 +0100

    feature: created basic layout

--- app/page.js
+++ app/page.js
@@ -4,14 +4,6 @@ export default function Home() {
   return (
     <main>
       <h1>Home</h1>
-      <p>
-        New Content Here: Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Consectetur id sed eligendi? Vero delectus impedit nesciunt in
-        ipsum unde! Reprehenderit blanditiis ipsam veritatis, consequatur
-        quibusdam quae facere laudantium a iste asperiores quaerat cum hic
-        dolores labore amet deserunt, repellat minus expedita nihil aut.
-        Voluptatum harum fugit ducimus eligendi id labore...
-      </p>
     </main>
   );
 }
